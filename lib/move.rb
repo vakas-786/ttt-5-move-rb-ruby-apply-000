3c0b4d6fd6d8bd53a67bf058db6790d483882e71
effective-model-0741 <vakhtar@fordham.edu>
2020-04-15 23:12:19 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,10 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-board = [" ", " ", " "]
-def  input_to_index(array, index, value)
-  array[index] = value
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, player="X")
+    board[index] = player
 end
